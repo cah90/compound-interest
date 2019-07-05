faa657488211d1ea5b9d7cad32b7186300b61c07
cassia <cascrisbern@gmail.com>
2019-07-05 14:14:02 +0100

    test

--- script.js
+++ script.js
@@ -1,39 +1,42 @@
-function calculateProfit(initialInvestment, monthlyInvestment, monthsNumber, profitRate) {
-    if (monthsNumber == 0) {
+function calculateProfit( initialInvestment, monthlyInvestment, monthsNumber, profitRate ) {
+    if ( monthsNumber == 0 ) {
         return 0;
     }
 
-    var totalProfitBeginningOfMonth = calculateProfit(initialInvestment, monthlyInvestment, monthsNumber-1, profitRate); 
-    var totalInvestmentBeginningOfMonth = initialInvestment + monthlyInvestment * (monthsNumber-1);
+    var totalProfitBeginningOfMonth = calculateProfit( initialInvestment, monthlyInvestment, monthsNumber - 1, profitRate );
+
+    var totalInvestmentBeginningOfMonth = initialInvestment + monthlyInvestment * ( monthsNumber - 1 );
 
     var sumBeginningOfMonth = totalInvestmentBeginningOfMonth + totalProfitBeginningOfMonth;
 
     var profitForMonth = sumBeginningOfMonth * profitRate;
-    
-    
+
     return totalProfitBeginningOfMonth + profitForMonth;
 }
 
 
 $( document ).ready( function () {
-    
+    $( ".btn" ).on( "click", function () {
+        $( ".header" ).css( "color", "red" );
 
         var initialInvestment, monthlyInvestment, monthsNumber;
 
-        initialInvestment = parseFloat($( "#initial-investment" ).val());
-        monthlyInvestment = parseFloat($( "#monthly-investment" ).val());
-        monthsNumber = parseFloat($( "#months-number" ).val());
-        yearlyInterest = parseFloat($("#yearly-interest").val());
+        initialInvestment = parseFloat( $( "#initial-investment" ).val() );
+        monthlyInvestment = parseFloat( $( "#monthly-investment" ).val() );
+        monthsNumber = parseFloat( $( "#months-number" ).val() );
+        yearlyInterest = parseFloat( $( "#yearly-interest" ).val() );
 
         var totalInitialInvestment = initialInvestment + monthlyInvestment * monthsNumber;
-        console.log(totalInitialInvestment);
-        
-        var monthlyInterest = yearlyInterest; // TODO
+        console.log( totalInitialInvestment );
 
-        profit = calculateProfit(initialInvestment, monthlyInvestment, monthsNumber, monthlyInterest );
-        console.log('profit', profit);
-        
+        var monthlyInterest = yearlyInterest;
+
+        profit = calculateProfit( initialInvestment, monthlyInvestment, monthsNumber, monthlyInterest );
+
     } );
 
+    
 
-} );    
+
+
+} );
